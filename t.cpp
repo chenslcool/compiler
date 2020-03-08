89137d850d75cbefe56af7e116a70beab4fd03c3
chenliang <1515904193@qq.com>
2020-03-08 11:19:24 +0800

    发现，负号和减号前面都有可能是),后者为if(..) -3;的情况，仅这一种，考虑flex中识别if处理

--- t.cpp
+++ t.cpp
@@ -1,7 +1,6 @@
- int func_test()
-{
-int _dec_ = 209;
-int _oct_ = 0115;
-int _dhex_ = 0x06Fab - _dec_number;
-int _result_ = - dhex + oct * ( dec - 0X000F );
+int main(){
+    int a = 2;
+    if(a > 2)
+        3;
+    return 0;
 }
